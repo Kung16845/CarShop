e535432423d284035e42e884c14b60dcd409b4e9
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-11-25 23:38:35 +0700

    AddmethodRandomOTP

--- Program/Program.cs
+++ Program/Program.cs
@@ -8,4 +8,16 @@
     {
         
     }
+    static void RandomOTP()
+    {
+        Random rnd = new Random();
+                int i = 1;
+                while (i <= 6)
+                {
+                    Console.Write(rnd.Next(0, 10));
+                    i++;
+                }
+                System.Threading.Thread.Sleep(5000);
+                Console.Clear();
+    }
 }
