91c9af02a7234a5573269cde2003ec8965f7f5f3
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-11-27 23:40:49 +0700

    UpDateMethod

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,20 +1,63 @@
-﻿public class Program 
+enum Menu 
+{   
+    MenuLogin = 1
+    ,MenuListCar
+}
+public class Program 
 {
     static Emaillist emaillist;
     static Persondlist persondlist;
+    static Vehiclelist vehiclelist;
 
-    static void PreparPersonListAndEmilList()
+    static void PreparInfo()
     {
         Program.emaillist = new Emaillist();
         Program.persondlist = new Persondlist();
+        Program.vehiclelist = new Vehiclelist();
+        InfoAdmin();
+        InfoCar();
     }
     static void Main(string[] args)
     {
-        PreparPersonListAndEmilList();
-        RegisterCustomer();       
+        PreparInfo();
+        MenuCar();   
+    }
+    static void MenuCar()
+    {
+        Console.WriteLine("Welcome To Car Rental Office");
+        Console.WriteLine(" 1 : MenuLogin \n 2 : MenuListCar");
+        Menu menu = (Menu)(int.Parse(Console.ReadLine()));
+        switch(menu)
+        {
+            case Menu.MenuLogin:
+                Login();
+                AdminMode();
+                break;
+            case Menu.MenuListCar:
+                vehiclelist.PrintInfoCar();
+                BookatCar(); 
+                break;
+            default :
+                break;
+        }
+    }
+    static void BookatCar()
+    {
+        Console.WriteLine("Choose Car in Catalogue");
+        Console.WriteLine("Back to MainMenu : 0");
+        int numbercar = int.Parse(Console.ReadLine());
+        if (numbercar == 0)
+        {
+            MenuCar();
+        }
+        // เลือกรถจอง 
+        RegisterCustomer();
+        Console.WriteLine("Recived Order!!!");
+        MenuCar();
     }
     static void RegisterCustomer()
     {
+        Console.Clear();
         Console.WriteLine("Type in Pronouns is Mr press 1, Mrs press 2 ,Miss press 3");
         string pronouns = Console.ReadLine();
         if (pronouns == "1" )
@@ -38,26 +81,56 @@
         Person persons = new Person(pronouns,name,surname,age);
         persondlist.AddNewPerson(persons);
     }  
+    static void AdminMode()
+    {
+        Console.Clear();
+        Console.WriteLine(" 1 : Signin New Worker \n 2 : Customers of Data \n 3 : Data All Worker \n 0 : Back To MainMenu");
+        int n = int.Parse(Console.ReadLine());
+        if (n == 1)
+        {
+            Signin();
+        }
+        else if (n == 2)
+        {
+            Output();
+        }
+        else if (n == 3)
+        {
+            //ข้อมูลพนักงาน 
+        }
+        else if (n == 0)
+        {
+            MenuCar();
+        }
+       
+    }
     static void Signin()
     {
+        Console.Clear();
+        Console.WriteLine("Sign-In new worker!!!");
         Email email = new Email(InputEmail(),InputPassword());
         if (emaillist.checkmail(email.GetEmail()) == "Alreadytanken") 
         {
             Console.WriteLine("Alreadytanken");
+            System.Threading.Thread.Sleep(2000);
             Signin();
         }
         else if (emaillist.checkmail(email.GetEmail()) == "e")
         {
-            Console.WriteLine("Menu");
+            MenuCar();
         }
         else 
         {
             Console.WriteLine("Thank You for Register!!!!");
             emaillist.AddNewEmail(email);
+            MenuCar();
         }       
     }
     static void Login()
     {      
+        Console.Clear();
+        Console.WriteLine("Login to AdminMode");
+        Console.WriteLine("Back to MainMenu : exit");
         Email email = new Email(InputEmail(),InputPassword());
         if (emaillist.login(email.GetEmail(),email.Getpassword()) == "Passed")
         {
@@ -66,29 +139,32 @@
         else if (emaillist.login(email.GetEmail(),email.Getpassword()) == "Deninded")
         {
             Console.WriteLine("Wrong Email or Wrong Password");
+            System.Threading.Thread.Sleep(2000);
             Login();
         }
         else 
         {
-            Console.WriteLine("Menu");
+            MenuCar();
         }
     }
+    static void InfoAdmin()
+    {
+        emaillist.AddNewEmail(new Email("11","22"));
+    }
+    static void InfoCar()
+    {
+        vehiclelist.AddNewVehicle(new Vehicleinfo("Fortuner","Toyota","SUV",5000));
+        vehiclelist.AddNewVehicle(new Vehicleinfo("Pajero Sports","Mitsubishi","SUV",5000));
+        vehiclelist.AddNewVehicle(new Vehicleinfo("City 1.5","Honda","SUV",5000));
+        vehiclelist.AddNewVehicle(new Vehicleinfo("Almera 1.2","Nissan","SUV",5000));
+        vehiclelist.AddNewVehicle(new Vehicleinfo("Mu-X","ISUZU ","SUV",5000));
+    }
+   
     static void Output()
     {
         persondlist.ShowPerson();
     }
-    static void RandomOTP()
-    {
-        Random rnd = new Random();
-                int i = 1;
-                while (i <= 6)
-                {
-                    Console.Write(rnd.Next(0, 10));
-                    i++;
-                }
-                System.Threading.Thread.Sleep(5000);
-                Console.Clear();
-    }
+   
     public static string InputEmail()
     {
         Console.WriteLine("Input your Email");
