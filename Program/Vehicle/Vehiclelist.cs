b5020c6bb3c88499a55d7870fd30fd7761e63151
Kung16845 <111451858+Kung16845@users.noreply.github.com>
2022-11-27 23:38:11 +0700

    addmanymenthod

--- Program/Vehicle/Vehiclelist.cs
+++ Program/Vehicle/Vehiclelist.cs
@@ -13,4 +13,15 @@ public class Vehiclelist
     {
         this.VehicleList.Add(vehicleinfo);
     }
+    public void PrintInfoCar()
+    {
+        Console.WriteLine("Information of ALLCAR");
+        int i = 1;
+        foreach(Vehicleinfo car in this.VehicleList)
+        {
+             Console.WriteLine("Car number : {0} \n Brand : {1} \n Model : {2} \n Type : {3} \n Rent Price : {4} Baht per Day",i,car.GetBrand(),car.GetModel(),car.GetCartype(),car.Getcarvalye());
+             Console.WriteLine("----------------------------------------");
+             i++;
+        }
+    }
 }
