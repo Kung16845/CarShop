84ed24cd65b83b5b1e10f3cd88b59bf0ee146f54
Pakhawat-Pongmanawut <110761105+Pakhawat-Pongmanawut@users.noreply.github.com>
2022-11-30 16:57:44 +0700

    Time Tester
    เขียนแล้ว แต่ลืมเอาเข้า Git ไม่ได้ใช้ไม่เป็นไร

---
+++ TimeTest/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+ class Program
+{
+    public int P = 0;
+    public int R = 0;
+    static void Main(string[] args)
+    {
+        string[] RentDate = new string[] 
+        {
+            "Clock",
+            "Rent Time Remaining"
+        };
+        for(int i=0; i < RentDate.Length; i++)
+        {
+            Console.WriteLine("{0}. {1}", i, RentDate[i]);
+        }
+        int u = 0;
+        Console.Write("Select Menu From Keyboard : ");
+        u = int.Parse(Console.ReadLine());
+        if(u == 0)
+        {
+             
+            int a = 99999;
+            long currenttime = DateTime.Now.Ticks;
+            Console.WriteLine(new DateTime(currenttime));
+            for(int i = 0; i < a; i--)
+            {
+            Console.Clear();
+            currenttime = currenttime + 10000000;
+            Console.WriteLine(new DateTime(currenttime));
+            System.Threading.Thread.Sleep(1000);
+            }
+        }
+        
+        else if (u == 1)
+        {
+            Console.WriteLine("Your Rent Duration is {0} Day(s)");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Command Not Found!");
+            Console.Clear();
+        }
+       
+    }
+    
+}
+       
